6e5e1dcb6d0da10ae508f400f6f4a033ca8bbca1
wachino <juanandc@ucm.es>
2021-04-04 13:11:02 +0200

    Change react components from Class to functions with hooks, update dependencies

--- src/App.js
+++ src/App.js
@@ -1,64 +1,70 @@
 import React from 'react';
-
 import {
   connectToBluetoothDevice,
   startNotifications,
-  disconnectFromBluetoothDevice
+  isWebBluetoothSupported,
+  disconnectFromBluetoothDevice,
 } from './helpers/bluetooth';
 import { parseCube } from './helpers/cubeParser';
-import * as SRVisualizer from 'sr-visualizer'
+import * as SRVisualizer from 'sr-visualizer';
 import './App.css';
 
 const faceColorMap = ['g', 'y', 'r', 'w', 'o', 'b'];
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { cubeState: 'bbbbbbbbboooooooooyyyyyyyyygggggggggrrrrrrrrrwwwwwwwww' };
-    this.device = null;
-  }
-  componentWillUnmount() {
-    disconnectFromBluetoothDevice(this.device);
-  }
-  
-  render() {
-    return (
-      <div className="App">        
-        <div>
-          <div id="imageContainer"></div>
-          <button
-            onClick={async () => {
+const App = () => {
+  const [cubeState, setCubeState] = React.useState(
+    'bbbbbbbbboooooooooyyyyyyyyygggggggggrrrrrrrrrwwwwwwwww'
+  );
+  const [device, setDevice] = React.useState(null);
 
-              // a quick check for bluetooth support in the browser
-              if(!('bluetooth' in navigator)){
-                alert("Browser does not support bluetooth");
-                return;
-              }
+  React.useEffect(() => {
+    // returned function will be called on component unmount
+    return () => {
+      disconnectFromBluetoothDevice(device);
+    };
+  }, [device]);
 
-              const { server, device } = await connectToBluetoothDevice();
-              this.device = device;
-              const characteristic = await startNotifications(server);
-              characteristic.addEventListener('characteristicvaluechanged', event => {
-                const { value } = event.target; // 20 bytes sent by the cube
-                const cubeState = parseCube(value) // We parse it to an array of 54 colors (1...6)
-                  .map(faceletColor => faceColorMap[faceletColor - 1])
-                  .join('');
-                this.setState({ cubeState });
-                const imgContainer = document.querySelector('#imageContainer'); 
-                imgContainer.innerHTML = "";
-                SRVisualizer.cubeSVG(imgContainer, `visualcube.php?fmt=svg&r=x-90y-120x-20&size=300&fc=${this.state.cubeState}`);
-              });
-              device.addEventListener('gattserverdisconnected', () => {
-                disconnectFromBluetoothDevice(device);
-              });
-            }}
-          >
-            connect
-          </button>
-        </div>
-      </div>
+  React.useEffect(() => {
+    const imgContainer = document.querySelector('#imageContainer');
+    imgContainer.innerHTML = '';
+    SRVisualizer.cubeSVG(
+      imgContainer,
+      `visualcube.php?fmt=svg&r=x-90y-120x-20&size=300&fc=${cubeState}`
     );
-  }
-}
+  }, [cubeState]);
+
+  return (
+    <div className="App">
+      <div>
+        <div id="imageContainer"></div>
+        <button
+          onClick={async () => {
+            // a quick check for bluetooth support in the browser
+            if (!isWebBluetoothSupported) {
+              alert('Browser does not support bluetooth');
+              return;
+            }
+
+            const { server, device: connectedDevice } = await connectToBluetoothDevice();
+            setDevice(connectedDevice);
+            const characteristic = await startNotifications(server);
+            characteristic.addEventListener('characteristicvaluechanged', (event) => {
+              const { value } = event.target; // 20 bytes sent by the cube
+              const newCubeState = parseCube(value) // We parse it to an array of 54 colors (1...6)
+                .map((faceletColor) => faceColorMap[faceletColor - 1])
+                .join('');
+              setCubeState(newCubeState);
+            });
+            connectedDevice.addEventListener('gattserverdisconnected', () => {
+              disconnectFromBluetoothDevice(connectedDevice);
+            });
+          }}
+        >
+          connect
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/helpers/bluetooth.js
+++ src/helpers/bluetooth.js
@@ -1,16 +1,16 @@
 const SERVICE_UUID = '0000aadb-0000-1000-8000-00805f9b34fb';
 const CHARACTERISTIC_UUID = '0000aadc-0000-1000-8000-00805f9b34fb';
 
-//export const isWebBluetoothSupported = 'bluetooth' in navigator;
+export const isWebBluetoothSupported = 'bluetooth' in navigator;
 
 export const connectToBluetoothDevice = () => {
   return navigator.bluetooth
     .requestDevice({
       acceptAllDevices: true,
-      optionalServices: [SERVICE_UUID]
+      optionalServices: [SERVICE_UUID],
     })
-    .then(device =>
-      device.gatt.connect().then(server => {
+    .then((device) =>
+      device.gatt.connect().then((server) => {
         window.mdevice = device;
         window.mserver = server;
         return { device, server };
@@ -18,17 +18,17 @@ export const connectToBluetoothDevice = () => {
     );
 };
 
-export const startNotifications = server =>
-  server.getPrimaryService(SERVICE_UUID).then(service => {
+export const startNotifications = (server) =>
+  server.getPrimaryService(SERVICE_UUID).then((service) => {
     window.mservice = service;
-    return service.getCharacteristic(CHARACTERISTIC_UUID).then(characteristic => {
+    return service.getCharacteristic(CHARACTERISTIC_UUID).then((characteristic) => {
       window.mcharacteristic = characteristic;
       characteristic.startNotifications();
       return characteristic;
     });
   });
 
-export const disconnectFromBluetoothDevice = device => {
+export const disconnectFromBluetoothDevice = (device) => {
   if (!device || !device.gatt.connected) return Promise.resolve();
   return device.gatt.disconnect();
 };
